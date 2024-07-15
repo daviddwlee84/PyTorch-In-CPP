53bcaef4ed77c47fcc21164dc38969a6e11911ae
Da-Wei Lee <dawei.lee@jingle.ai>
2024-07-15 20:40:19 +0800

    Inference model with Tensor

--- 1_TorchScript/example/2_TorchScript_Convert/README.md
+++ 1_TorchScript/example/2_TorchScript_Convert/README.md
@@ -1,3 +1,5 @@
 
+- [Loading a TorchScript Model in C++ — PyTorch Tutorials 2.3.0+cu121 documentation](https://pytorch.org/tutorials/advanced/cpp_export.html)
+
 1. `./build.sh`
 2. `./example-app ../traced_resnet_model.pt`
--- 1_TorchScript/example/2_TorchScript_Convert/example-app.cpp
+++ 1_TorchScript/example/2_TorchScript_Convert/example-app.cpp
@@ -23,5 +23,13 @@ int main(int argc, const char *argv[])
         return -1;
     }
 
-    std::cout << "ok\n";
+    std::cout << "successfully loaded the model\n";
+
+    // Create a vector of inputs.
+    std::vector<torch::jit::IValue> inputs;
+    inputs.push_back(torch::ones({1, 3, 224, 224}));
+
+    // Execute the model and turn its output into a tensor.
+    at::Tensor output = module.forward(inputs).toTensor();
+    std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
 }
