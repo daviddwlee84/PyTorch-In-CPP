371759357810ea812d63ed9a6dded9278489858a
Da-Wei Lee <dawei.lee@jingle.ai>
2024-07-15 20:21:33 +0800

    Add basic example of convert torch model by tracing and load it

---
+++ 1_TorchScript/example/2_TorchScript_Convert/README.md
@@ -0,0 +1,3 @@
+
+1. `./build.sh`
+2. `./example-app ../traced_resnet_model.pt`
---
+++ 1_TorchScript/example/2_TorchScript_Convert/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir build
+cd build
+
+# Absolute Path
+# NOTE: Need to use C++17 or later to compile (ATen & PyTorch)
+# cmake -DCMAKE_PREFIX_PATH=/absolute/path/to/libtorch -DCMAKE_CXX_STANDARD=17 ..
+cmake -DCMAKE_PREFIX_PATH=$(pwd)/../../../../libtorch -DCMAKE_CXX_STANDARD=17 ..
+# Use PIP installed PyTorch
+# cmake -DCMAKE_PREFIX_PATH=`python -c 'import torch;print(torch.utils.cmake_prefix_path)'` ..
+
+cmake --build . --config Release
