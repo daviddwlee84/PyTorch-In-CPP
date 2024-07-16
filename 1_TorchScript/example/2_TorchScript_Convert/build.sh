9786b2cc26590c4615fe1f549128c1f636b68c9b
Da-Wei Lee <dawei.lee@jingle.ai>
2024-07-16 15:20:29 +0800

    Make tqdm.hpp a shared include among projects

--- 1_TorchScript/example/2_TorchScript_Convert/build.sh
+++ 1_TorchScript/example/2_TorchScript_Convert/build.sh
@@ -6,7 +6,8 @@ cd build
 # Absolute Path
 # NOTE: Need to use C++17 or later to compile (ATen & PyTorch)
 # cmake -DCMAKE_PREFIX_PATH=/absolute/path/to/libtorch -DCMAKE_CXX_STANDARD=17 ..
-cmake -DCMAKE_PREFIX_PATH=$(pwd)/../../../../libtorch -DCMAKE_CXX_STANDARD=17 ..
+# cmake -DCMAKE_PREFIX_PATH=$(pwd)/../../../../libtorch -DCMAKE_CXX_STANDARD=17 ..
+cmake -DCMAKE_PREFIX_PATH=/mnt/NAS/sda/ShareFolder/lidawei/library/libtorch -DCMAKE_CXX_STANDARD=17 ..
 # Use PIP installed PyTorch
 # cmake -DCMAKE_PREFIX_PATH=`python -c 'import torch;print(torch.utils.cmake_prefix_path)'` ..
 
