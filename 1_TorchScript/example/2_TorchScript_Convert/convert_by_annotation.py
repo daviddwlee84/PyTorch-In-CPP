57255e561aebf36fc3754d71d7fb115b57230683
Da-Wei Lee <dawei.lee@jingle.ai>
2024-07-16 09:50:11 +0800

    Add benchmark result and model summary

--- 1_TorchScript/example/2_TorchScript_Convert/convert_by_annotation.py
+++ 1_TorchScript/example/2_TorchScript_Convert/convert_by_annotation.py
@@ -1,10 +1,16 @@
 import torch
 import torchvision
+from torchinfo import summary
+import time
 
 # An instance of your model.
 model = torchvision.models.resnet18()
 
 # Use torch.jit.script to generate a torch.jit.ScriptModule via annotation.
+start = time.perf_counter()
 script_module = torch.jit.script(model)
+print("Generate via tracing cost:", time.perf_counter() - start, "seconds")
 
-script_module.save("script_resnet_model.pt")
+script_module.save("scripted_resnet_model.pt")
+
+summary(model)
--- 1_TorchScript/example/2_TorchScript_Convert/convert_by_tracing.py
+++ 1_TorchScript/example/2_TorchScript_Convert/convert_by_tracing.py
@@ -1,5 +1,7 @@
 import torch
 import torchvision
+from torchinfo import summary
+import time
 
 # An instance of your model.
 model = torchvision.models.resnet18()
@@ -7,7 +9,12 @@ model = torchvision.models.resnet18()
 # An example input you would normally provide to your model's forward() method.
 example = torch.rand(1, 3, 224, 224)
 
+start = time.perf_counter()
 # Use torch.jit.trace to generate a torch.jit.ScriptModule via tracing.
 traced_script_module = torch.jit.trace(model, example)
+print("Generate via tracing cost:", time.perf_counter() - start, "seconds")
 
 traced_script_module.save("traced_resnet_model.pt")
+
+# RuntimeError: Only one of (input_data, input_size) should be specified.
+summary(model, input_data=example)
