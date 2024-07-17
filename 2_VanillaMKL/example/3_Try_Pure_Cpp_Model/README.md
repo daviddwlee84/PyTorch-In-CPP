b7e046118860c9395580f8cbbb58057754792cad
Da-Wei Lee <dawei.lee@jingle.ai>
2024-07-17 13:58:10 +0800

    Able to limit the threads by environment variable

--- 2_VanillaMKL/example/3_Try_Pure_Cpp_Model/README.md
+++ 2_VanillaMKL/example/3_Try_Pure_Cpp_Model/README.md
@@ -1,8 +1,28 @@
+# Build Pure C++ GRU Model Using oneMKL Library
 
-```
+```bash
 mkdir build
 cd build
 cmake ..
 make cpu-gpu
-./pure_cpp
+```
+
+```bash
+# Default
+(research) ➜  build git:(main) ✗ ./pure_cpp 
+MKL is using 60 threads.
+OpenMP is using 120 threads.
+Output: 2.41577
+
+# Try MKL_NUM_THREADS
+(research) ➜  build git:(main) ✗ MKL_NUM_THREADS=1 ./pure_cpp
+MKL is using 1 threads.
+OpenMP is using 120 threads.
+Output: 2.41577
+
+# Try OMP_NUM_THREADS
+(research) ➜  build git:(main) ✗ OMP_NUM_THREADS=1 ./pure_cpp 
+MKL is using 1 threads.
+OpenMP is using 1 threads.
+Output: 2.41577
 ```
--- 2_VanillaMKL/example/3_Try_Pure_Cpp_Model/src/main.cpp
+++ 2_VanillaMKL/example/3_Try_Pure_Cpp_Model/src/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <cstdlib>
 #include "SingleStepLSTMRegressionMKL.h"
+#include <mkl.h>
+#include <omp.h>
 
 int main()
 {
@@ -9,6 +11,14 @@ int main()
     int64_t hidden_size = 64; // Example hidden size
     int64_t num_layers = 2;   // Example number of layers
     SingleStepLSTMRegressionMKL model(feature_dim, hidden_size, num_layers);
+
+    // Print the number of threads being used
+    int mkl_threads = mkl_get_max_threads();
+    std::cout << "MKL is using " << mkl_threads << " threads." << std::endl;
+
+    // Print the number of CPUs being used (OpenMP)
+    int omp_threads = omp_get_max_threads();
+    std::cout << "OpenMP is using " << omp_threads << " threads." << std::endl;
 
     // Example input
     std::vector<float> input(feature_dim, 1.0); // Batch size 1, sequence length 1, feature dimension
