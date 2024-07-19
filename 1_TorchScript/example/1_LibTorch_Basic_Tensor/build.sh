70c4d57f81d5bb9b6453dadfaaaea846ca27d9de
Da-Wei Lee <dawei.lee@jingle.ai>
2024-07-19 10:00:32 +0800

    CMake VSCode debug example

--- 1_TorchScript/example/1_LibTorch_Basic_Tensor/build.sh
+++ 1_TorchScript/example/1_LibTorch_Basic_Tensor/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Default to Release if no argument is provided
+BUILD_TYPE=${1:-Release}
+
 mkdir build
 cd build
 
@@ -7,6 +10,6 @@ cd build
 # cmake -DCMAKE_PREFIX_PATH=/absolute/path/to/libtorch ..
 # cmake -DCMAKE_PREFIX_PATH=$(pwd)/../../../../libtorch ..
 # Use PIP installed PyTorch (make sure we are using same version torch for tensor dump and load)
-cmake -DCMAKE_PREFIX_PATH=`python -c 'import torch;print(torch.utils.cmake_prefix_path)'` ..
+cmake -DCMAKE_PREFIX_PATH=`python -c 'import torch;print(torch.utils.cmake_prefix_path)'` -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ..
 
-cmake --build . --config Release
+cmake --build . --config ${BUILD_TYPE}
